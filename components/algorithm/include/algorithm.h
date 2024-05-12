9fd51f2d7f257620602651c3573aad80332dee37
likko <kikkoliu69@gmail.com>
2024-05-12 18:54:40 +0800

    FFT 头文件引入成功

---
+++ components/algorithm/include/algorithm.h
@@ -0,0 +1,19 @@
+#ifndef __AIGORITHM_H_
+#define __AIGORITHM_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/spi_master.h"
+#include "soc/gpio_struct.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "soc/uart_struct.h"
+#include <math.h>
+
+#include "esp_dsp.h"
+
+#endif
